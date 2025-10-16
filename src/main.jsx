56c345601460a1f08008122031b7ab2dfe3b7c5d
zavarikhin-dv <zavarikhin-dv@zvuk.com>
2025-10-16 12:56:52 +0300

    init project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@hexlet/testing-task-manager';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {App()}
+  </React.StrictMode>
+);
